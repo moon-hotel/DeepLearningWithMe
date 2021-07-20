607af51c59c5ed4c631ccaa06c2cec64ebb064c7
moon-hotel <moon-hotel@hotmail.com>
2021-07-20 18:37:24 +0800

    加入模型保存载入和追加训练

---
+++ 04_ModelPersistence/01_TrainAndSave/train.py
@@ -0,0 +1,85 @@
+import torchvision
+import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
+from LeNet5 import LeNet5
+import os
+
+
+def load_dataset(batch_size):
+    mnist_train = torchvision.datasets.FashionMNIST(root='~/Datasets/FashionMNIST',
+                                                    train=True, download=True,
+                                                    transform=transforms.ToTensor())
+    mnist_test = torchvision.datasets.FashionMNIST(root='~/Datasets/FashionMNIST',
+                                                   train=False, download=True,
+                                                   transform=transforms.ToTensor())
+    train_iter = torch.utils.data.DataLoader(mnist_train,
+                                             batch_size=batch_size,
+                                             shuffle=True,
+                                             num_workers=1)
+    test_iter = torch.utils.data.DataLoader(mnist_test,
+                                            batch_size=batch_size,
+                                            shuffle=True,
+                                            num_workers=1)
+    return train_iter, test_iter
+
+
+class MyModel:
+    def __init__(self,
+                 batch_size=64,
+                 epochs=5,
+                 learning_rate=0.001,
+                 model_save_dir='./MODEL'):
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.model_save_dir = model_save_dir
+        self.net = LeNet5()
+
+    def train(self):
+        train_iter, test_iter = load_dataset(self.batch_size)
+
+        loss = nn.CrossEntropyLoss(reduction='mean')
+        if not os.path.exists(self.model_save_dir):
+            os.makedirs(self.model_save_dir)
+        model_save_path = os.path.join(self.model_save_dir, 'model.pkl')
+        if os.path.exists(model_save_path):
+            loaded_paras = torch.load(model_save_path)
+            self.net.load_state_dict(loaded_paras)
+            print("#### 成功载入已有模型，进行追加训练...")
+
+        optimizer = torch.optim.Adam(self.net.parameters(), lr=self.learning_rate)  # 定义优化器
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.net.to(device)
+        for epoch in range(self.epochs):
+            for i, (x, y) in enumerate(train_iter):
+                x, y = x.to(device), y.to(device)
+                logits = self.net(x)
+                l = loss(logits, y)
+                optimizer.zero_grad()
+                l.backward()
+                optimizer.step()  # 执行梯度下降
+                if i % 100 == 0:
+                    acc = (logits.argmax(1) == y).float().mean()
+                    print("Epochs[{}/{}]---batch[{}/{}]---acc {:.4}---loss {:.4}".format(
+                        epoch, self.epochs, len(train_iter) // self.batch_size, i, acc, l))
+
+            print("Epochs[{}/{}]--acc on test {:.4}".format(epoch, self.epochs,
+                                                            self.evaluate(test_iter, self.net, device)))
+            torch.save(self.net.state_dict(), model_save_path)
+
+    @staticmethod
+    def evaluate(data_iter, net, device):
+        with torch.no_grad():
+            acc_sum, n = 0.0, 0
+            for x, y in data_iter:
+                x, y = x.to(device), y.to(device)
+                logits = net(x)
+                acc_sum += (logits.argmax(1) == y).float().sum().item()
+                n += len(y)
+            return acc_sum / n
+
+
+if __name__ == '__main__':
+    model = MyModel()
+    model.train()
