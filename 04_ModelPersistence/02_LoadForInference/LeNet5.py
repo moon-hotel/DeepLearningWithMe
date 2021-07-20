eaf7dddb1bc4bea41ce93eecf547fc98d398dbfd
moon-hotel <moon-hotel@hotmail.com>
2021-07-20 18:39:01 +0800

    加入模型载入和进行推理

---
+++ 04_ModelPersistence/02_LoadForInference/LeNet5.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+import torch
+
+
+class LeNet5(nn.Module):
+    def __init__(self, ):
+        super(LeNet5, self).__init__()
+        self.conv = nn.Sequential(  # [n,1,28,28]
+            nn.Conv2d(1, 6, 5, padding=2),  # in_channels, out_channels, kernel_size
+            nn.ReLU(),  # [n,6,24,24]
+            nn.MaxPool2d(2, 2),  # kernel_size, stride  [n,6,14,14]
+            nn.Conv2d(6, 16, 5),  # [n,16,10,10]
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)  # [n,16,5,5]
+        )
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(16 * 5 * 5, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, img):
+        output = self.conv(img)
+        output = self.fc(output)
+        return output
---
+++ 04_ModelPersistence/02_LoadForInference/inference.py
@@ -0,0 +1,47 @@
+import torchvision
+import torchvision.transforms as transforms
+import torch
+from LeNet5 import LeNet5
+import os
+
+
+def load_dataset(batch_size):
+    mnist_train = torchvision.datasets.FashionMNIST(root='~/Datasets/FashionMNIST',
+                                                    train=True, download=True,
+                                                    transform=transforms.ToTensor())
+    mnist_test = torchvision.datasets.FashionMNIST(root='~/Datasets/FashionMNIST',
+                                                   train=False, download=True,
+                                                   transform=transforms.ToTensor())
+    train_iter = torch.utils.data.DataLoader(mnist_train,
+                                             batch_size=batch_size,
+                                             shuffle=True,
+                                             num_workers=1)
+    test_iter = torch.utils.data.DataLoader(mnist_test,
+                                            batch_size=batch_size,
+                                            shuffle=True,
+                                            num_workers=1)
+    return train_iter, test_iter
+
+
+def inference(data_iter, device, model_save_dir='./MODEL'):
+    net = LeNet5()  # 初始化现有模型的权重参数
+    net.to(device)
+    model_save_path = os.path.join(model_save_dir, 'model.pkl')
+    if os.path.exists(model_save_path):
+        loaded_paras = torch.load(model_save_path)
+        net.load_state_dict(loaded_paras)  # 用本地已有模型来重新初始化网络权重参数
+        net.eval()
+    with torch.no_grad():
+        acc_sum, n = 0.0, 0
+        for x, y in data_iter:
+            x, y = x.to(device), y.to(device)
+            logits = net(x)
+            acc_sum += (logits.argmax(1) == y).float().sum().item()
+            n += len(y)
+        print("Accuracy in test data is :", acc_sum / n)
+
+
+if __name__ == '__main__':
+    train_iter, test_iter = load_dataset(64)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    inference(test_iter, device)
