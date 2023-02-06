4b3c0e014ba5e52d5d6cd4357847b15e203221c9
moon-hotel <moon-hotel@hotmail.com>
2023-02-06 20:02:29 +0800

    添加分类模型top-k实现代码

--- Code/Chapter03/C14_ClaMetrics/main.py
+++ Code/Chapter03/C14_ClaMetrics/main.py
@@ -7,6 +7,23 @@
 """
 
 from sklearn.metrics import classification_report
+import torch
+
+
+def calculate_top_k_accuracy(logits, targets, k=2):
+    """
+    计算top-k准确率
+    :param logits: shape: (m,n)
+    :param targets:  shape: (m,)
+    :param k:
+    :return:
+    """
+    values, indices = torch.topk(logits, k=k, sorted=True)
+    y = torch.reshape(targets, [-1, 1])
+    correct = (y == indices) * 1.  # 对比预测的K个值中是否包含有正确标签中的结果
+    top_k_accuracy = torch.mean(correct) * k  # 计算最后的准确率
+    return top_k_accuracy
+
 
 if __name__ == '__main__':
     y_true = [1, 1, 1, 0, 0, 0, 2, 2, 2, 2]
@@ -14,3 +31,9 @@ if __name__ == '__main__':
     result = classification_report(y_true, y_pred,
                                    target_names=['class 0', 'class 1', 'class 2'])
     print(result)
+
+    logits = torch.tensor([[0.1, 0.3, 0.2, 0.4],
+                           [0.5, 0.01, 0.9, 0.4]])
+    y = torch.tensor([3, 0])
+    print(calculate_top_k_accuracy(logits, y, k=1).item())  # 0.5
+    print(calculate_top_k_accuracy(logits, y, k=2).item())  # 1.0
