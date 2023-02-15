77e103957c1d380d8c4a8b20c2927248b98922b6
moon-hotel <moon-hotel@hotmail.com>
2023-02-15 20:26:30 +0800

    加入第3.10.12节Dropout代码和内容

--- Code/Chapter03/C17_Dropout/main.py
+++ Code/Chapter03/C17_Dropout/main.py
@@ -8,31 +8,45 @@
 
 import torch._refs as refs
 import torch
+import torch.nn as nn
 
 
-def dropout(a, p=0.5, training=True):
+def dropout(a, drop_pro=0.5, training=True):
     """
     参考：torch/_refs/nn/functional/__init__.py
     :param a:
-    :param p:
-    :param training:
+    :param drop_pro: 丢弃率
+    :param training: 是否处于训练状态
     :return:
     """
     if not training:
         return a
-    assert p <= 1
-    assert p >= 0
-    if p == 1:
+    assert 0 <= drop_pro <= 1
+    if drop_pro == 1:
         return refs.zeros_like(a)
-    if p == 0:
+    if drop_pro == 0:
         return a
-    p1m = 1 - p
-    scale = 1 / p1m
-    mask = refs.uniform(a.shape, low=0.0, high=1.0, dtype=torch.float32, device=a.device) < p1m
+    keep_pro = 1 - drop_pro
+    scale = 1 / keep_pro
+    mask = refs.uniform(a.shape, low=0.0, high=1.0, dtype=torch.float32, device=a.device) < keep_pro
     return refs.mul(refs.mul(a, mask), scale)
+
+
+class MyDropout(nn.Module):
+    def __init__(self, p=0.5):
+        super(MyDropout, self).__init__()
+        self.p = p
+
+    def forward(self, x):
+        return dropout(x, drop_pro=self.p, training=self.training)
 
 
 if __name__ == '__main__':
     a = torch.randn([2, 10])
-    y = dropout(a, p=0.4)
-    print(y)
+    op_dropout = MyDropout(p=0.2)
+    print(op_dropout(a))
+
+# tensor([[-1.4843, -1.0103, -0.0000,  0.7997,  0.9650,  0.4117, -0.6568, -0.4334,
+#           1.5951, -0.9222],
+#         [-1.2151,  2.4469, -1.9339, -0.6010, -0.0000,  0.0342, -1.1552,  0.0000,
+#           0.1395,  1.7941]])
