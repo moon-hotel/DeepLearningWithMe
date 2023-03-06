3022f87a3506c253af2b9dd730185553a5a32833
moon-hotel <moon-hotel@hotmail.com>
2023-03-06 21:00:57 +0800

    添加运行结果

--- Code/Chapter05/C04_ModelSaving/E01_CheckParams.py
+++ Code/Chapter05/C04_ModelSaving/E01_CheckParams.py
@@ -15,3 +15,15 @@ if __name__ == '__main__':
     print("Model's state_dict:")
     for (name, param) in model.state_dict().items():
         print(name, param.size())
+
+    # Model's state_dict:
+    # conv.0.weight torch.Size([6, 1, 5, 5])
+    # conv.0.bias torch.Size([6])
+    # conv.3.weight torch.Size([16, 6, 5, 5])
+    # conv.3.bias torch.Size([16])
+    # fc.1.weight torch.Size([120, 400])
+    # fc.1.bias torch.Size([120])
+    # fc.3.weight torch.Size([84, 120])
+    # fc.3.bias torch.Size([84])
+    # fc.5.weight torch.Size([10, 84])
+    # fc.5.bias torch.Size([10])
