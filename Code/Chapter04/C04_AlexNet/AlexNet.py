d89e078cd798930d54c5b0bc831a7c0e03aa49c6
moon-hotel <moon-hotel@hotmail.com>
2023-03-22 20:46:09 +0800

    添加AlexNet前向传播实现

---
+++ Code/Chapter04/C04_AlexNet/AlexNet.py
@@ -0,0 +1,57 @@
+"""
+文件名: Code/Chapter04/C03_LeNet5/LeNet5.py
+创建时间: 2023/3/22 8:29 下午
+作 者: @空字符
+公众号: @月来客栈
+知 乎: @月来客栈 https://www.zhihu.com/people/the_lastest
+"""
+
+import torch.nn as nn
+import torch
+
+
+class AlexNet(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1000, dropout=0.5):
+        super(AlexNet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=96, kernel_size=11, stride=4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # kernel_size, stride
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        # 这里全连接层的输出个数比LeNet5中的大数倍。使用了丢弃层Dropout来缓解过拟合
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=256 * 5 * 5, out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features=4096, out_features=num_classes))
+
+    def forward(self, img, labels=None):
+        feature = self.conv(img)
+        logits = self.fc(feature)
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss(reduction='mean')
+            loss = loss_fct(logits, labels)
+            return loss, logits
+        else:
+            return logits
+
+
+if __name__ == '__main__':
+    model = AlexNet(in_channels=1)
+    x = torch.rand(32, 1, 224, 224)
+    out = model(x)
+    print(out.shape) # torch.Size([32, 1000])
