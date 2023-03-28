e7a697f54d85ea1cb561c088d54937996ded4b2b
moon-hotel <moon-hotel@hotmail.com>
2023-03-28 20:58:54 +0800

    加入VGG训练代码

---
+++ Code/Chapter04/C05_VGG/train.py
@@ -0,0 +1,128 @@
+"""
+文件名: Code/Chapter04/C05_VGG/train.py
+创建时间: 2023/3/27 19:55 下午
+作 者: @空字符
+公众号: @月来客栈
+知 乎: @月来客栈 https://www.zhihu.com/people/the_lastest
+"""
+
+from torchvision.datasets import CIFAR10
+import torchvision.transforms as transforms
+from torchvision.transforms import InterpolationMode
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import torch
+from VGG import vgg
+import logging
+import sys
+import os
+from copy import deepcopy
+
+sys.path.append("../../")
+from utils import logger_init
+
+
+class ModelConfig(object):
+    def __init__(self, ):
+        self.batch_size = 64
+        self.epochs = 5
+        self.learning_rate = 0.001
+        self.in_channels = 3
+        self.num_classes = 10
+        self.resize = 224  # 将输入的28*28的图片，resize成224*224的形状
+        self.vgg_type = 'A'
+        self.num_classes = 10
+        self.init_weights = True
+        self.model_save_path = 'vgg11.pt'
+        self.summary_writer_dir = "runs/vgg11"
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        # 判断是否存在GPU设备，其中0表示指定第0块设备
+        logger_init(log_file_name='vgg', log_level=logging.INFO, log_dir='log')
+        logging.info("### 将当前配置打印到日志文件中 ")
+        for key, value in self.__dict__.items():
+            logging.info(f"### {key} = {value}")
+
+
+def load_dataset(config, is_train=True):
+    trans = [transforms.ToTensor()]
+    if config.resize:  # 将输入的32*32的图片，resize成224*224的形状
+        trans.append(transforms.Resize(size=config.resize,
+                                       interpolation=InterpolationMode.BILINEAR))
+    trans = transforms.Compose(trans)
+    dataset = CIFAR10(root='~/Datasets/CIFAR10', train=is_train,
+                      download=True, transform=trans)
+    iter = DataLoader(dataset, batch_size=config.batch_size, shuffle=True)
+    return iter
+
+
+def train(config):
+    train_iter = load_dataset(config, is_train=True)
+    test_iter = load_dataset(config, is_train=False)
+    model = vgg(config)
+    if os.path.exists(config.model_save_path):
+        logging.info(f" # 载入模型{config.model_save_path}进行追加训练...")
+        checkpoint = torch.load(config.model_save_path)
+        model.load_state_dict(checkpoint)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+    writer = SummaryWriter(config.summary_writer_dir)
+    model = model.to(config.device)
+    max_test_acc = 0
+    model.train()
+    global_steps = 0
+    for epoch in range(config.epochs):
+        for i, (x, y) in enumerate(train_iter):
+            x, y = x.to(config.device), y.to(config.device)
+            loss, logits = model(x, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()  # 执行梯度下降
+            global_steps += 1
+            if i % 50 == 0:
+                acc = (logits.argmax(1) == y).float().mean()
+                logging.info(f"Epochs[{epoch + 1}/{config.epochs}]--batch[{i}/{len(train_iter)}]"
+                             f"--Acc: {round(acc.item(), 4)}--loss: {round(loss.item(), 4)}")
+                writer.add_scalar('Training/Accuracy', acc, global_steps)
+            writer.add_scalar('Training/Loss', loss.item(), global_steps)
+        test_acc = evaluate(test_iter, model, config.device)
+        logging.info(f"Epochs[{epoch + 1}/{config.epochs}]--Acc on test {test_acc}")
+        writer.add_scalar('Testing/Accuracy', test_acc, global_steps)
+        if test_acc > max_test_acc:
+            max_test_acc = test_acc
+            state_dict = deepcopy(model.state_dict())
+            torch.save(state_dict, config.model_save_path)
+
+
+def evaluate(data_iter, model, device):
+    model.eval()
+    with torch.no_grad():
+        acc_sum, n = 0.0, 0
+        for x, y in data_iter:
+            x, y = x.to(device), y.to(device)
+            logits = model(x)
+            acc_sum += (logits.argmax(1) == y).float().sum().item()
+            n += len(y)
+        return acc_sum / n
+
+
+def inference(config, test_iter):
+    model = vgg(config)
+    model.to(config.device)
+    if os.path.exists(config.model_save_path):
+        logging.info(f" # 载入模型进行推理……")
+        checkpoint = torch.load(config.model_save_path)
+        model.load_state_dict(checkpoint)
+    else:
+        raise ValueError(f" # 模型{config.model_save_path}不存在！")
+    first_batch = next(iter(test_iter))
+    with torch.no_grad():
+        logits = model(first_batch[0].to(config.device))
+    y_pred = logits.argmax(1)
+    print(f"真实标签为：{first_batch[1]}")
+    print(f"预测标签为：{y_pred}")
+
+
+if __name__ == '__main__':
+    config = ModelConfig()
+    model = train(config)
+    # test_iter = load_dataset(config, is_train=False)
+    # inference(config, test_iter)
