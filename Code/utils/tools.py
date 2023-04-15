b4d062b30cf6a4f560a124996a1a1d2f72aa981f
moon-hotel <moon-hotel@hotmail.com>
2023-04-15 10:48:59 +0800

    优化gpu获取代码，支持以id获取

--- Code/utils/tools.py
+++ Code/utils/tools.py
@@ -1,19 +1,27 @@
 """
 文件名: Code/utils/tools.py
-创建时间: 2023/4/12 4:27 下午
+创建时间: 2023/4/12 8:27 下午
+作 者: @空字符
+公众号: @月来客栈
+知 乎: @月来客栈 https://www.zhihu.com/people/the_lastest
 """
 
 import torch
 
 
-def get_gpus(num=10):
+def get_gpus(num=None):
     """
-    如果有GPU则返回前num个gpu
+    如果num为list，则返回list中对应的GPU；
+    如果num为整数，则返回前num个gpu
     如果没有GPU则返回CPU
     :param num:
     :return:
     """
-
-    devices = [torch.device(f'cuda:{i}')
-               for i in range(torch.cuda.device_count())][:num]
+    gpu_nums = torch.cuda.device_count()
+    if isinstance(num, list):
+        devices = [torch.device(f'cuda:{i}')
+                   for i in num if i < gpu_nums]
+    else:
+        devices = [torch.device(f'cuda:{i}')
+                   for i in range(gpu_nums)][:num]
     return devices if devices else [torch.device('cpu')]
