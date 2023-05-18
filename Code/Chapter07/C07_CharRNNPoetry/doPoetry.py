d09ff91a61c1f17c3a21349f326ebe37211ecde7
moon-hotel <moon-hotel@hotmail.com>
2023-05-18 19:47:13 +0800

    优化代码修复bug

--- Code/Chapter07/C07_CharRNNPoetry/doPoetry.py
+++ Code/Chapter07/C07_CharRNNPoetry/doPoetry.py
@@ -6,6 +6,7 @@
 知 乎: @月来客栈 https://www.zhihu.com/people/the_lastest
 """
 from CharRNN import CharRNN
+from train import ModelConfig
 import logging
 import os
 import torch
@@ -24,21 +25,21 @@ def greedy_decode(model, src, config):
     :param config:
     :return:
     """
-    max_len = [5 * config.num_sens, 7 * config.num_sens]  # 五言 或 七言
-    src = src.to(config.device)  # 初始时刻的输入形状通常应该是[1,1]，即只有一个字，但是也可以输入一句诗
+    max_len = [12 * config.num_sens, 16 * config.num_sens]  # 五言 或 七言 5*2+2， 7*2+2
+    src = src.to(config.device)  # 初始时刻的输入形状通常应该是[1,1]，即只有一个字，但是也可以输入一句诗[1,n]
     for i in range(max(max_len) - 1):
-        out = model(src)  # [1,src_len,vocab_size]
-        _, next_word = torch.max(out[:, -1], dim=1)  # 每次在最后一个时刻的输出结果 中选择概率最大者
-        next_word = next_word.item()
-        src = torch.cat([src, torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=0)
+        out = model(src)  # [batch_size, src_len, vocab_size]
+        prob = torch.softmax(out[:, -1], dim=-1)  # 计算得到最后一个时刻输出结果的概率分布
+        next_word = torch.distributions.Categorical(prob).sample().item()  # 得到下一个词
+        src = torch.cat([src, torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
         # 将当前时刻解码的预测输出结果，同之前所有的结果堆叠作为输入再去预测下一个词。
-        if len(src.shape[1]) in max_len:
+        if (src.shape[1] == min(max_len)):
             break
     return src
 
 
 def inference(config, srcs=None):
-    model = CharRNN(config.vocab_size, config.embedding_size,
+    model = CharRNN(config.top_k, config.embedding_size,
                     config.hidden_size, config.num_layers, config.cell_type,
                     config.bidirectional)
     model.to(config.device)
@@ -55,8 +56,12 @@ def inference(config, srcs=None):
     with torch.no_grad():
         for src in srcs:
             result = greedy_decode(model, src, config)
-            print(result)
+            result = [tang_shi.vocab.itos[item.item()] for item in result[0]]
+            print("".join(result))
 
-    # y_pred = logits.argmax(1)
-# print(f"真实标签为：{first_batch[1]}")
-# print(f"预测标签为：{y_pred}")
+
+if __name__ == '__main__':
+    config = ModelConfig()
+    config.__dict__['num_sens'] = 4
+    srcs = ["李白乘舟将欲行", "朝辞白帝彩"]
+    inference(config, srcs)
