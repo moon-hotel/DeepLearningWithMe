7ca0d7bf969339ad025289dc45e91dbd4a6c10ce
nulls8 <moon-hotel@hotmail.com>
2023-06-27 20:41:48 +0800

    优化网络结构代码

--- Code/Chapter08/C07_STResNet/STResNet.py
+++ Code/Chapter08/C07_STResNet/STResNet.py
@@ -8,7 +8,6 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class ResUnit(nn.Module):
@@ -27,15 +26,15 @@ class ResUnit(nn.Module):
 
 
 class ResComponent(nn.Module):
-    def __init__(self, conv1_in_chs=8, conv1_out_chs=16, num_unit=3, res_out_chs=32, num_flow=2):
+    def __init__(self, conv1_in_chs=8, conv1_out_chs=16, num_res_unit=3, res_out_chs=32, nb_flow=2):
         super().__init__()
 
         self.conv1 = nn.Conv2d(conv1_in_chs, conv1_out_chs, 3, stride=1, padding=1)
         res_units = []
-        for i in range(num_unit):
+        for i in range(num_res_unit):
             res_units.append(ResUnit(conv1_out_chs, res_out_chs))
         self.res_units = nn.ModuleList(res_units)
-        self.conv2 = nn.Conv2d(conv1_out_chs, num_flow, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(conv1_out_chs, nb_flow, 3, stride=1, padding=1)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -46,45 +45,45 @@ class ResComponent(nn.Module):
 
 
 class FeatureExt(nn.Module):
-    def __init__(self, ext_dim=20, num_flow=2, map_height=32, map_width=32):
+    def __init__(self, ext_dim=20, nb_flow=2, map_height=32, map_width=32):
         super().__init__()
-        self.num_flow = num_flow
+        self.nb_flow = nb_flow
         self.map_height = map_height
         self.map_width = map_width
 
         self.feature = nn.Sequential(
             nn.Linear(ext_dim, 10),
             nn.ReLU(inplace=True),
-            nn.Linear(10, num_flow * map_height * map_width),
+            nn.Linear(10, nb_flow * map_height * map_width),
             nn.ReLU(inplace=True))
 
     def forward(self, x):
         x = self.feature(x.to(torch.float32))
-        x = torch.reshape(x, [-1, self.num_flow, self.map_height, self.map_width])
+        x = torch.reshape(x, [-1, self.nb_flow, self.map_height, self.map_width])
         return x
 
 
 class STResNet(nn.Module):
     def __init__(self, config=None):
         super().__init__()
-        self.trend = ResComponent(config.num_flow * config.len_closeness,
-                                  config.conv1_out_chs, config.num_unit, config.res_out_chs)
-        self.period = ResComponent(config.num_flow * config.len_period,
-                                   config.conv1_out_chs, config.num_unit, config.res_out_chs)
-        self.close = ResComponent(config.num_flow * config.len_trend,
-                                  config.conv1_out_chs, config.num_unit, config.res_out_chs)
-        self.w_t = nn.Parameter(torch.randn([1, config.num_flow, config.map_height, config.map_width]))
-        self.w_p = nn.Parameter(torch.randn([1, config.num_flow, config.map_height, config.map_width]))
-        self.w_c = nn.Parameter(torch.randn([1, config.num_flow, config.map_height, config.map_width]))
-        self.ext_feature = FeatureExt(config.ext_dim, config.num_flow, config.map_height, config.map_width)
+        self.close = ResComponent(config.nb_flow * config.len_closeness,
+                                  config.conv1_out_chs, config.num_res_unit, config.res_out_chs)
+        self.period = ResComponent(config.nb_flow * config.len_period,
+                                   config.conv1_out_chs, config.num_res_unit, config.res_out_chs)
+        self.trend = ResComponent(config.nb_flow * config.len_trend,
+                                  config.conv1_out_chs, config.num_res_unit, config.res_out_chs)
+        self.w_t = nn.Parameter(torch.randn([1, config.nb_flow, config.map_height, config.map_width]))
+        self.w_p = nn.Parameter(torch.randn([1, config.nb_flow, config.map_height, config.map_width]))
+        self.w_c = nn.Parameter(torch.randn([1, config.nb_flow, config.map_height, config.map_width]))
+        self.ext_feature = FeatureExt(config.ext_dim, config.nb_flow, config.map_height, config.map_width)
 
     def forward(self, x, y=None):
-        x1 = self.trend(x[0])
-        x2 = self.period(x[1])
-        x3 = self.close(x[2])
-        y1 = x1 * self.w_t + x2 * self.w_p + x3 * self.w_c
-        y2 = self.ext_feature(x[3])
-        logits = F.tanh(y1 + y2)
+        x0 = self.close(x[0])
+        x1 = self.period(x[1])
+        x2 = self.trend(x[2])
+        x3 = self.ext_feature(x[3])
+        y1 = x0 * self.w_c + x1 * self.w_p + x2 * self.w_t
+        logits = torch.tanh(y1 + x3)
         if y is not None:
             loss_fct = nn.MSELoss()
             loss = loss_fct(logits, y)
@@ -95,29 +94,29 @@ class STResNet(nn.Module):
 
 class ModelConfig(object):
     def __init__(self):
-        self.num_flow = 2  #
+        self.nb_flow = 2  #
         self.len_closeness = 3
         self.len_period = 1
         self.len_trend = 1
         self.conv1_out_chs = 64
-        self.res_out_chs = 128
-        self.num_unit = 4
+        self.res_out_chs = 64
+        self.num_res_unit = 12
         self.map_height = 32
         self.map_width = 32
-        self.ext_dim = 18
+        self.ext_dim = 28
 
 
 if __name__ == '__main__':
     config = ModelConfig()
-    x0 = torch.randn([16, config.num_flow * config.len_closeness, config.map_height, config.map_width])
-    x1 = torch.randn([16, config.num_flow * config.len_period, config.map_height, config.map_width])
-    x2 = torch.randn([16, config.num_flow * config.len_trend, config.map_height, config.map_width])
+    x0 = torch.randn([16, config.nb_flow * config.len_closeness, config.map_height, config.map_width])
+    x1 = torch.randn([16, config.nb_flow * config.len_period, config.map_height, config.map_width])
+    x2 = torch.randn([16, config.nb_flow * config.len_trend, config.map_height, config.map_width])
     x3 = torch.randint(0, 2, size=[16, config.ext_dim])
-    y = torch.randn([16, config.num_flow, config.map_height, config.map_width])
+    y = torch.randn([16, config.nb_flow, config.map_height, config.map_width])
     x = [x0, x1, x2, x3]
     st = STResNet(config)
     print(st(x).shape)
-    loss, logits = st(x,y)
+    loss, logits = st(x, y)
     print(logits.shape)
     print(loss)
-
+    print(st)
