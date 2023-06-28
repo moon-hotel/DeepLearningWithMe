332308fc4cfe3159f8657b1a310c8bd488ca55b8
nulls8 <moon-hotel@hotmail.com>
2023-06-28 23:55:50 +0800

    添加模型评估和推理部分的代码

--- Code/Chapter08/C07_STResNet/train.py
+++ Code/Chapter08/C07_STResNet/train.py
@@ -5,7 +5,7 @@
 公众号: @月来客栈
 知 乎: @月来客栈 https://www.zhihu.com/people/the_lastest
 """
-
+import numpy as np
 from transformers import optimization
 from torch.utils.tensorboard import SummaryWriter
 import torch
@@ -14,7 +14,6 @@ import sys
 import logging
 import os
 from STResNet import STResNet
-import torchvision.transforms as transforms
 
 sys.path.append("../../")
 from utils import TaxiBJ
@@ -22,16 +21,18 @@ from utils import TaxiBJ
 
 class ModelConfig(object):
     def __init__(self):
-        self.num_flow = 2  #
+        self.nb_flow = 2  #
         self.len_closeness = 3
         self.len_period = 1
         self.len_trend = 1
         self.conv1_out_chs = 64
-        self.res_out_chs = 128
-        self.num_unit = 4
+        self.res_out_chs = 64
+        self.num_res_unit = 4
         self.map_height = 32
         self.map_width = 32
-        self.ext_dim = 18
+        self.ext_dim = 28
+        self.T = 48
+        self.len_test = self.T * 4 * 7  # 4周一共28天作为测试集
         self.batch_size = 64
         self.epochs = 50
         self.learning_rate = 4e-3
@@ -46,8 +47,8 @@ class ModelConfig(object):
 
 
 def train(config):
-    data_loader = TaxiBJ(len_test=config.len_test, len_closeness=config.len_closeness,
-                         len_period=config.len_period, len_trend=config.len_trend)
+    data_loader = TaxiBJ(config.T, config.nb_flow, config.len_test, config.len_closeness,
+                         config.len_period, config.len_trend, batch_size=config.batch_size)
     train_iter = data_loader.load_train_test_data(is_train=True)
     model = STResNet(config)
     if os.path.exists(config.model_save_path):
@@ -57,51 +58,60 @@ def train(config):
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     writer = SummaryWriter(config.summary_writer_dir)
     model = model.to(config.device)
-    max_test_acc = 0
     steps = len(train_iter) * config.epochs
     scheduler = optimization.get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=config.num_warmup_steps,
                                                              num_training_steps=steps, num_cycles=2)
+    max_train_loss = 10000.
     for epoch in range(config.epochs):
-        for i, (XC_train, XP_train, XT_train, Y_train, _, _) in enumerate(train_iter):
-            XC_train = XC_train.to(config.device)
-            XP_train = XP_train.to(config.device)
-            XT_train = XT_train.to(config.device)
-            Y_train = Y_train.to(config.device)
-            loss, logits = model([XC_train, XP_train, XT_train], Y_train)
+        total_loss = 0
+        for i, (XC, XP, XT, Y, meta_feature, _) in enumerate(train_iter):
+            XC = XC.to(config.device)
+            XP = XP.to(config.device)
+            XT = XT.to(config.device)
+            Y = Y.to(config.device)
+            meta_feature = meta_feature.to(config.device)
+            loss, logits = model([XC, XP, XT, meta_feature], Y)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()  # 执行梯度下降
             scheduler.step()
+            total_loss += loss.item()
             if i % 50 == 0:
                 logging.info(f"Epochs[{epoch + 1}/{config.epochs}]--batch[{i}/{len(train_iter)}]"
                              f"--loss: {round(loss.item(), 4)}")
                 writer.add_scalar('Training/Loss', loss.item(), scheduler.last_epoch)
-        # test_acc = evaluate(train_iter, model, config.device)
-        # logging.info(f"Epochs[{epoch + 1}/{config.epochs}]--Acc on val {test_acc}")
-        # writer.add_scalar('Testing/Accuracy', test_acc, scheduler.last_epoch)
-        # if test_acc > max_test_acc:
-        #     max_test_acc = test_acc
-        #     state_dict = deepcopy(model.state_dict())
-        #     torch.save(state_dict, config.model_save_path)
+        logging.info(f"Epochs[{epoch + 1}/{config.epochs}] --Total loss: {total_loss}")
+        if total_loss < max_train_loss:
+            max_train_loss = total_loss
+            state_dict = deepcopy(model.state_dict())
+            torch.save(state_dict, config.model_save_path)
+            inference(config)
 
 
 def evaluate(data_iter, model, device):
     model.eval()
+    all_logits = []
+    all_labels = []
     with torch.no_grad():
-        acc_sum, n = 0.0, 0
-        for x, y in data_iter:
-            x, y = x.to(device), y.to(device)
-            logits = model(x)
-            acc_sum += (logits.argmax(1) == y).float().sum().item()
-            n += len(y)
+        for i, (XC, XP, XT, Y, meta_feature_test, _) in enumerate(data_iter):
+            XC = XC.to(device)
+            XP = XP.to(device)
+            XT = XT.to(device)
+            Y = Y.to(device)
+            meta_feature_test = meta_feature_test.to(device)
+            loss, logits = model([XC, XP, XT, meta_feature_test], Y)
+            all_logits.append(logits)
+            all_labels.append(Y)
         model.train()
-        return acc_sum / n
+        all_logits = torch.cat(all_logits, dim=0)
+        all_labels = torch.cat(all_labels, dim=0)
+    return all_logits, all_labels
 
 
-def inference(config, ):
-    data_loader = TaxiBJ(len_test=config.len_test, len_closeness=config.len_closeness,
-                         len_period=config.len_period, len_trend=config.len_trend)
-    test_iter = data_loader.load_train_test_data(is_train=False)
+def inference(config):
+    data_loader = TaxiBJ(config.T, config.nb_flow, config.len_test, config.len_closeness,
+                         config.len_period, config.len_trend, batch_size=config.batch_size)
+    test_iter, mmn = data_loader.load_train_test_data(is_train=False)
     model = STResNet(config)
     model.to(config.device)
     model.eval()
@@ -111,12 +121,14 @@ def inference(config, ):
         model.load_state_dict(checkpoint)
     else:
         raise ValueError(f" # 模型{config.model_save_path}不存在！")
-    first_batch = next(iter(test_iter))
-    with torch.no_grad():
-        logits = model(first_batch[0].permute(0, 2, 1, 3, 4).to(config.device))
-    y_pred = logits.argmax(1)
-    logging.info(f"真实标签为：{first_batch[1]}")
-    logging.info(f"预测标签为：{y_pred}")
+
+    all_logits, all_labels = evaluate(test_iter, model, config.device)
+    y_pred = all_logits.detach().cpu().numpy()  # [1344, 2,32,32]
+    y_true = all_labels.detach().cpu().numpy()  # [1344, 2,32,32]
+    y_pred = mmn.inverse_transform(y_pred)
+    y_true = mmn.inverse_transform(y_true)
+    rmse = np.sqrt(np.mean((y_pred - y_true) ** 2))
+    logging.info(f" # RMSE on test: {rmse}")
 
 
 if __name__ == '__main__':
