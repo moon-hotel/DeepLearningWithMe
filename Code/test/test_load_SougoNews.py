ba864fcd5ca566fb770447293b1287ef57cef3f1
nulls8 <moon-hotel@hotmail.com>
2023-07-14 23:05:29 +0800

    添加示例代码

--- Code/test/test_load_SougoNews.py
+++ Code/test/test_load_SougoNews.py
@@ -4,13 +4,14 @@ import unicodedata
 sys.path.append('../')
 
 from utils import SougoNews
-import time
+from utils import MyCorpus
+
 if __name__ == '__main__':
     # model = SougoNews()
     # model.data_process()
-    # text = "（沪Ａ：６００２２１）"
-    # text = unicodedata.normalize('NFKC', text)
-    # print(text)
-    for i in range(5, 0, -1):
-        print(f"Countdown: {i}s")
-        time.sleep(1)
+
+    sentences = MyCorpus()
+    for item in sentences:
+        print(item)
+        break
+
