a43a43ca1b8450331333da4d7ddd8e939da4f2c4
nulls8 <moon-hotel@hotmail.com>
2023-07-18 20:49:55 +0800

    添加作者信息

--- Code/Chapter09/C03_GloVe/main.py
+++ Code/Chapter09/C03_GloVe/main.py
@@ -1,3 +1,11 @@
+"""
+文件名: Code/Chapter09/C03_GloVe/main.py
+创建时间: 2023/7/17 19:54 下午
+作 者: @空字符
+公众号: @月来客栈
+知 乎: @月来客栈 https://www.zhihu.com/people/the_lastest
+"""
+
 from gensim.scripts.glove2word2vec import glove2word2vec
 from gensim.models import KeyedVectors
 import logging
