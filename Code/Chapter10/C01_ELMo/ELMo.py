fcc321efdb7b0f49fd4354d2b40dfd6f06b941fb
nulls8 <moon-hotel@hotmail.com>
2023-08-27 15:53:09 +0800

    加入ELMo前向传播实现

---
+++ Code/Chapter10/C01_ELMo/ELMo.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import logging
+
+
+# {"lstm": {"use_skip_connections": true, "projection_dim": 512, "cell_clip": 3, "proj_clip": 3, "dim": 4096,
+#           "n_layers": 2},
+#  "char_cnn": {"activation": "relu", "filters": [[1, 32], [2, 32], [3, 64], [4, 128], [5, 256], [6, 512], [7, 1024]],
+#               "n_highway": 2, "embedding": {"dim": 16}, "n_characters": 262, "max_characters_per_token": 50}}
+
+class ModelConfig(object):
+    def __init__(self):
+        super().__init__()
+        self.char_embed_num = 262
+        self.char_embed_dim = 16
+        self.max_characters_per_token = 50
+        self.char_cnn_filters = [[1, 32], [2, 32], [3, 64],
+                                 [4, 128], [5, 256], [6, 512], [7, 1024]]
+        self.n_filters = sum(f[1] for f in self.char_cnn_filters)
+        self.projection_dim = 512
+        self.n_layers = 2
+
+
+class ELMoCharacterCNN(nn.Module):
+    """
+    ELMo Character CNN  得到每个单词的token embedding
+    """
+
+    def __init__(self, config=None):
+        super(ELMoCharacterCNN, self).__init__()
+        self.config = config
+        self.char_embedding = nn.Embedding(config.char_embed_num,
+                                           config.char_embed_dim)
+        conv = []
+        # char_cnn_filters: [[1, 32], [2, 32], [3, 64], [4, 128], [5, 256], [6, 512], [7, 1024]]
+        for i, (width, num) in enumerate(config.char_cnn_filters):
+            conv.append(nn.Conv1d(in_channels=config.char_embed_dim,
+                                  out_channels=num, kernel_size=width, bias=True))
+        self.char_conv = nn.ModuleList(conv)
+        self.highway = nn.Linear(config.n_filters, config.n_filters * 2)
+        self.projection = nn.Linear(config.n_filters, config.projection_dim)
+
+    def forward(self, x):
+        """
+        :param x: shape: [batch_size, seq_len, max_chars_per_token]
+        :return: token_embedding: shape: [batch_size, seq_len, projection_dim]
+        """
+        seq_len = x.shape[1]
+        x = self.char_embedding(x)  # [batch_size, seq_len, max_chars_per_token, char_embed_dim]
+        x = x.reshape(-1, x.shape[2], x.shape[3])  # [batch_size*seq_len, max_chars_per_token, char_embed_dim]
+        x = x.transpose(1, 2)  # [batch_size*seq_len, char_embed_dim, max_chars_per_token]
+        convs = []
+        for conv in self.char_conv:
+            convolved = conv(x)  # [batch_size*seq_len, n_filters_of_each_cnn, max_chars_per_token - width + 1]
+            convolved, _ = torch.max(convolved, dim=-1)  # 在最后一个维度，即特征通道这个维度上取最大池化，
+            convolved = F.relu(convolved)  # [batch_size*seq_len, n_filters_of_each_cnn]
+            convs.append(convolved)
+        # Highway
+        token_embedding = torch.cat(convs, dim=-1)  # linear_part, [batch_size * seq_len, n_filters]
+        highway = self.highway(token_embedding)  # 分别计算后面的非线性部分和门 [batch_size * seq_len, n_filters*2]
+        nonlinear_part, gate = highway.chunk(2, dim=-1)  # 两部分的形状均为 [batch_size * seq_len, n_filters]
+        nonlinear_part = F.relu(nonlinear_part)  # [batch_size * seq_len, n_filters]
+        gate = torch.sigmoid(gate)  # [batch_size * seq_len, n_filters]
+        token_embedding = gate * token_embedding + (1 - gate) * nonlinear_part
+        # [batch_size * seq_len, n_filters]
+        token_embedding = self.projection(token_embedding)  # [batch_size * seq_len, projection_dim]
+        token_embedding = token_embedding.view(-1, seq_len, self.config.projection_dim)
+        return token_embedding  # [batch_size, seq_len, projection_dim]
+
+
+class ELMoBiLSTM(nn.Module):
+    def __init__(self, config=None):
+        super().__init__()
+        self.config = config
+        forward_layers, back_layers = [], []
+        for _ in range(config.n_layers):
+            lstm_forward = nn.LSTM(input_size=config.projection_dim, hidden_size=config.projection_dim,
+                                   num_layers=1, batch_first=True)
+            lstm_backward = nn.LSTM(input_size=config.projection_dim, hidden_size=config.projection_dim,
+                                    num_layers=1, batch_first=True)
+            forward_layers.append(lstm_forward)
+            back_layers.append(lstm_backward)
+        self.forward_layers = nn.ModuleList(forward_layers)
+        self.back_layers = nn.ModuleList(back_layers)
+
+    def forward(self, x):
+        """
+
+        :param x:  shape: [batch_size, seq_len, projection_dim]
+        :return: [([batch_size, seq_len, projection_dim]), ([batch_size, seq_len, projection_dim]),...]
+        """
+        forward_cache, backward_cache = x, x.flip(1)
+        outputs = [torch.cat([forward_cache, backward_cache], dim=-1)]  # token embedding
+        for layer_id in range(self.config.n_layers):
+            forward_output = self.forward_layers[layer_id](forward_cache)[0]  # [batch_size, seq_len, projection_dim]
+            backward_output = self.back_layers[layer_id](backward_cache)[0]  # [batch_size, seq_len, projection_dim]
+            if layer_id != 0:  # skip-connection 第一层没有残差
+                forward_output += forward_cache
+                backward_output += backward_cache
+            outputs.append(torch.cat([forward_output, backward_output], dim=-1))
+            forward_cache = forward_output  # [batch_size, seq_len, projection_dim]
+            backward_cache = backward_output  # [batch_size, seq_len, projection_dim]
+        return outputs
+
+
+class ELMo(nn.Module):
+    def __init__(self, config=None, rep_weights=None):
+        """
+
+        :param config:
+        :param rep_weight: 每一层的特征表示的权重，list长度必须为 config.n_layers + 1
+        """
+        super().__init__()
+        self.config = config
+        self.char_cnn = ELMoCharacterCNN(config)
+        self.lstm = ELMoBiLSTM(config)
+        rep_weights_shape = [config.n_layers + 1, 1, 1, 1]
+        if rep_weights is None or len(rep_weights) != config.n_layers + 1:
+            if rep_weights is not None and len(rep_weights) != config.n_layers + 1:
+                logging.warning(f"rep_weights指定无效，其长度必须为{config.n_layers + 1}")
+            self.rep_weights = nn.Parameter(torch.randn(rep_weights_shape))
+        else:
+            self.rep_weights = torch.tensor(rep_weights).reshape(rep_weights_shape)
+
+    def forward(self, x):
+        """
+
+        :param x: [batch_size, seq_len, max_chars_per_token]
+        :return:
+        """
+        char_embedding = self.char_cnn(x)  # # [batch_size, seq_len, projection_dim]
+        outputs = self.lstm(char_embedding)
+        # [([batch_size, seq_len, projection_dim*2]), ([batch_size, seq_len, projection_dim*2]),...]
+        outputs = torch.stack(outputs, dim=0)  # [n_layers+1, batch_size, seq_len, projection_dim*2]
+        elmo_rep = (outputs * self.rep_weights).sum(0)  # [batch_size, seq_len, projection_dim*2]
+        return outputs, elmo_rep
---
+++ Code/Chapter10/C01_ELMo/test.py
@@ -0,0 +1,47 @@
+from ELMo import ELMoCharacterCNN
+from ELMo import ELMoBiLSTM
+from ELMo import ELMo
+import torch
+
+
+class ModelConfig(object):
+    def __init__(self):
+        super().__init__()
+        self.char_embed_num = 262
+        self.char_embed_dim = 16
+        self.max_characters_per_token = 50
+        self.char_cnn_filters = [[1, 32], [2, 32], [3, 64],
+                                 [4, 128], [5, 256], [6, 512], [7, 1024]]
+        self.n_filters = sum(f[1] for f in self.char_cnn_filters)
+        self.projection_dim = 512
+        self.n_layers = 2
+
+
+def test_ELMoCharacterCNN():
+    config = ModelConfig()
+    model = ELMoCharacterCNN(config)
+    token_ids = torch.randint(0, 100, [2, 6, 50])
+    print(model(token_ids).shape)
+
+
+def test_ELMoBiLSTM():
+    config = ModelConfig()
+    token_embedding = torch.rand([2, 6, config.projection_dim])
+    model = ELMoBiLSTM(config)
+    for i in range(config.n_layers + 1):
+        print("layer: ", i, model(token_embedding)[i].shape)
+
+
+def test_ELMo():
+    config = ModelConfig()
+    token_ids = torch.randint(0, 100, [2, 6, 50])
+    model = ELMo(config)
+    outputs, elmo_rep = model(token_ids)
+    print(outputs.shape)  # [n_layers+1, batch_size, seq_len, projection_dim*2]
+    print(elmo_rep.shape)  # [batch_size, seq_len, projection_dim]
+
+
+if __name__ == '__main__':
+    # test_ELMoCharacterCNN()
+    # test_ELMoBiLSTM()
+    test_ELMo()
