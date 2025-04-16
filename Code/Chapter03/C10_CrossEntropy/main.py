b7c585493462dcf53889660b6cc00c901926977e
nulls8 <moon-hotel@hotmail.com>
2025-04-16 19:14:09 +0800

    补充手动计算交叉熵代码

--- Code/Chapter03/C10_CrossEntropy/main.py
+++ Code/Chapter03/C10_CrossEntropy/main.py
@@ -7,10 +7,27 @@
 """
 import torch.nn as nn
 import torch
+import numpy as np
+
+
+def crossEntropy(y_true, logits):
+    loss = y_true * np.log(logits)  # [m,n]
+    return -np.sum(loss) / len(y_true)
+
+
+def softmax(x):
+    s = np.exp(x)
+    return s / np.sum(s, axis=1, keepdims=True)
+
 
 if __name__ == '__main__':
     logits = torch.tensor([[0.5, 0.3, 0.6], [0.5, 0.4, 0.3]])
     y = torch.LongTensor([2, 0])
     loss = nn.CrossEntropyLoss(reduction='mean')  # 返回的均值是除以的每一批样本的个数（不一定是batch_size）
     l = loss(logits, y)
-    print(l)
+    print(l)  # tensor(0.9874)
+
+    logits = np.array([[0.5, 0.3, 0.6], [0.5, 0.4, 0.3]])
+    y = np.array([2, 0])
+    y_one_hot = np.eye(3)[y]
+    print(crossEntropy(y_one_hot, softmax(logits)))  # 0.9874308806774512
